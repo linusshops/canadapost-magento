97470653b407b40a059144dead6cdf4dbd984904
Sam Schmidt <samuel@dersam.net>
2015-12-10 14:15:10 -0500

    Add async gmaps loader.

--- src/skin/frontend/base/default/js/linuscanadapost/d2po.js
+++ src/skin/frontend/base/default/js/linuscanadapost/d2po.js
@@ -1,3 +1,27 @@
-/**
- * Created by samuelschmidt on 2015-12-10.
- */
+var linus = linus || {};
+
+linus.d2po = linus.d2po || (function($){
+    'use strict';
+
+    function makeMap($target, options)
+    {
+        return new google.maps.Map($target[0], options);
+    }
+
+    function init(apiKey, callback)
+    {
+        $.when(
+            $.ajax({
+                url: "https://maps.googleapis.com/maps/api/js?key="+apiKey+"&callback=initMap",
+                dataType: 'script',
+                cache: true,
+                crossDomain: true
+            })
+        ).done(callback);
+    }
+
+    return {
+        init: init,
+        makeMap: makeMap
+    };
+})(jQuery);
