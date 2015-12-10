58245adaaefc0b013316dfdb2987a343092480a1
Sam Schmidt <samuel@dersam.net>
2015-12-10 12:17:07 -0500

    Basic magento module structure created.

---
+++ src/app/code/community/Linus/CanadaPost/Helper/Data.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Helper
+ *
+ */
+class Linus_CanadaPost_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
---
+++ src/app/code/community/Linus/CanadaPost/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Index Controller
+ *
+ */
+class Linus_CanadaPost_IndexController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * Index Action
+     */
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
