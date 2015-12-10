31e1828e944cdf2394ae1504b1f0a6a80ebb667d
Sam Schmidt <samuel@dersam.net>
2015-12-10 14:25:57 -0500

    Add google maps config entry.

---
+++ src/app/code/community/Linus/CanadaPost/controllers/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Index Controller
+ *
+ */
+class Linus_CanadaPost_OfficeController extends Mage_Core_Controller_Front_Action
+{
+    public function nearestAction()
+    {
+
+    }
+
+    public function detailAction()
+    {
+
+    }
+}
