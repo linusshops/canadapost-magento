f153d23ac119e3d74c8591078500426c27ecea64
Sam Schmidt <samuel@dersam.net>
2015-12-10 16:15:22 -0500

    Add offices request.

--- src/app/code/community/Linus/CanadaPost/controllers/ApiController.php
+++ src/app/code/community/Linus/CanadaPost/controllers/ApiController.php
@@ -12,7 +12,40 @@ class Linus_CanadaPost_OfficeController extends Mage_Core_Controller_Front_Actio
      */
     public function nearestAction()
     {
+        $common = Mage::helper('linus_common/request');
+
+        //Require: Postal code
+        $postalCode = $this->getRequest()->get('postal_code');
+        $city = $this->getRequest()->get('city');
+        $province = $this->getRequest()->get('province');
+
+        if (in_array(null, array($postalCode, $city, $province))) {
+            $msg = '';
+
+            if ($postalCode == null) {
+                $msg .= 'postal code ';
+            }
+
+            if ($city == null) {
+                $msg .= 'city ';
+            }
+
+            if ($province == null) {
+                $msg .= 'province ';
+            }
+
+            $common->sendResponseJson(array(), 'Missing required fields: '.$msg);
+            return;
+        }
+
         /** @var GetNearestPostOffice $service */
         $service = Mage::helper('linus_canadapost')->getService('GetNearestPostOffice');
+        $offices = $service
+            ->setParameter('d2po', 'true')
+            ->setParameter('postalCode', $postalCode)
+            ->setParameter('city', $city)
+            ->setParameter('province', $province)
+            ->send()
+        ;
     }
 }
