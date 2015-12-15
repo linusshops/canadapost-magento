9f396d09ef625e815b19e2e64538d3786b906ca3
Sam Schmidt <samuel@dersam.net>
2015-12-15 12:14:02 -0500

    Comments.

--- src/skin/frontend/base/default/js/linuscanadapost/demo.js
+++ src/skin/frontend/base/default/js/linuscanadapost/demo.js
@@ -1,8 +1,8 @@
 jQuery(document).ready( function() {
     jQuery('#demo-map').on('click', function (event) {
         linus.canadapost.d2po.render(
-            '#map',
-            {postalCode: 'H4G1J9', city: 'Montreal', province: 'QC'}
+            '#map', //The target div to render your map to.
+            {postalCode: 'H4G1J9', city: 'Montreal', province: 'QC'} //All of these options are required.
         );
     });
 
@@ -10,11 +10,11 @@ jQuery(document).ready( function() {
 
     $map.on('onOfficeMarkerClick', function(event, office){
         console.log(event);
-        console.log(office);
+        console.log(office); //The office object represented by the marker. Contains all office data.
     });
 
     $map.on('onOfficesLoaded', function(event, offices){
         console.log(event);
-        console.log(offices);
+        console.log(offices); //An array of objects that contain office data.
     });
 });
