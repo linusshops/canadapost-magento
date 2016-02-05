4d3918527fd1bd1a11c1a48cfc1bff66a3086819
Sam Schmidt <samuel@dersam.net>
2016-02-05 11:53:47 -0500

    Comment d2po.js.

--- src/skin/frontend/base/default/js/linuscanadapost/d2po.js
+++ src/skin/frontend/base/default/js/linuscanadapost/d2po.js
@@ -1,26 +1,77 @@
 var linus = linus || {};
 linus.canadapost = linus.canadapost || {};
 
+/**
+ * Provides a mashup between Canada Post office locator api and Google Maps.
+ * Will automatically apply pins to the map in the location of Canada Post's
+ * closest offices to the center of the map.
+ *
+ * @author Sam Schmidt <samuel@dersam.net>
+ */
 linus.canadapost.d2po = linus.canadapost.d2po || (function($, _, Common)
 {
     'use strict';
 
+    /**
+     * Is the Google Maps Javascript api already loaded to the page?
+     * @type {boolean}
+     */
     var loaded = false;
+
+    /**
+     * The api key to use to access the GMaps api. Will use the one set in the
+     * Magento admin if available.
+     * @type {null}
+     */
     var apiKey = null;
+
+    /**
+     * The Google Map to which the post office pins are applied. This library
+     * assumes you will only have one D2PO map on the page, as there doesn't
+     * seem to be any use case for more than one.
+     */
     var map;
 
+    /**
+     * The maximum number of offices to be returned from Canada Post.
+     * @type {number}
+     */
     var maxOffices = 10;
 
+    /**
+     * Contains the currently active Canada Post markers.
+     * @type {Array}
+     */
     var markers = [];
+
+    /**
+     * The currently opened marker window.
+     * @type {null}
+     */
     var openedInfoWindow = null;
 
     var offices;
 
-    //Milliseconds to wait before updating office locations after dragging map.
+    /**
+     * Milliseconds to wait before updating office locations after dragging map.
+     * @type {int}
+     */
     var dragQueryDelay = 650;
+
+    /**
+     * Updated when dragstart fires, indicates that the map is currently being
+     * dragged. Used in the map update delay to determine if the delayed
+     * action should actually be used.
+     * @type {boolean}
+     */
     var isMapCurrentlyDragging = false;
     var lastTimer = null;
 
+    /**
+     * Set a specific api key. If you have set this in the Magento admin, not
+     * necessary to do it again.
+     * @param key
+     */
     function setApiKey(key)
     {
         apiKey = key;
@@ -31,6 +82,10 @@ linus.canadapost.d2po = linus.canadapost.d2po || (function($, _, Common)
         return apiKey;
     }
 
+    /**
+     * Only load the google maps library once rendering is requested.
+     * @returns {*}
+     */
     function lazyLoadGoogleMapsLibrary()
     {
         if (getApiKey() == null) {
@@ -66,7 +121,11 @@ linus.canadapost.d2po = linus.canadapost.d2po || (function($, _, Common)
         });
     }
 
-    //Get the lat/long coordinates for the postal code
+    /**
+     * Get the lat/long coordinates for the postal code
+     * @param postalCode
+     * @param {function} callback - action to take once the coordinates are acquired.
+     */
     function getPostalCodeCoordinates(postalCode, callback)
     {
         var geocoder = new google.maps.Geocoder();
@@ -74,10 +133,10 @@ linus.canadapost.d2po = linus.canadapost.d2po || (function($, _, Common)
     }
 
     /**
-     * Load the google maps library, then center on the give area code
+     * Load the google maps library, then center on the given area code
      * @param $target
-     * @param epicenter
-     * @returns {*}
+     * @param {postalCode, city, province} epicenter
+     * @returns {promise}
      */
     function createMap($target, epicenter)
     {
@@ -100,6 +159,9 @@ linus.canadapost.d2po = linus.canadapost.d2po || (function($, _, Common)
                         });
 
                         map.addListener('dragend', function(){
+                            //If there is a delay timer running, wipe it.
+                            //We've dragged somewhere else, so we don't care
+                            //about the previous end.
                             clearTimeout(lastTimer);
                             lastTimer = _.delay(onDragEnd, dragQueryDelay)
                         });
@@ -108,6 +170,9 @@ linus.canadapost.d2po = linus.canadapost.d2po || (function($, _, Common)
             });
     }
 
+    /**
+     * Update the map after the user finishes dragging the map.
+     */
     function onDragEnd()
     {
         //Don't bother querying if user has resumed panning in the delay.
@@ -216,7 +281,10 @@ linus.canadapost.d2po = linus.canadapost.d2po || (function($, _, Common)
     }
 
     /**
-     * Wrap map creation (if not already created) and adding markers
+     * Wrap map creation (if not already created) and adding markers.
+     *
+     * If you simply want to move the map, reposition() is more performant. Only
+     * use render when the map does not yet exist in the target.
      */
     function render(target, epicenter, apiKey)
     {
@@ -234,6 +302,11 @@ linus.canadapost.d2po = linus.canadapost.d2po || (function($, _, Common)
             });
     }
 
+    /**
+     * Repositions the map to a new epicenter, then looks up nearby Canada Post
+     * offices.
+     * @param epicenter
+     */
     function reposition(epicenter)
     {
         getPostalCodeCoordinates(epicenter.postalCode, function(results, status){
@@ -245,6 +318,9 @@ linus.canadapost.d2po = linus.canadapost.d2po || (function($, _, Common)
         });
     }
 
+    /**
+     * Obliterate all markers on the map.
+     */
     function clearAllMarkers()
     {
         _.forEach(markers, function(marker){
@@ -262,11 +338,19 @@ linus.canadapost.d2po = linus.canadapost.d2po || (function($, _, Common)
         return map;
     }
 
+    /**
+     * Set the maximum number of Canada Post offices to display.
+     * @param {int} max
+     */
     function setMaxOffices(max)
     {
         maxOffices = max;
     }
 
+    /**
+     * Set the delay between finishing dragging and the api lookup firing.
+     * @param {int} milliseconds
+     */
     function setDragQueryDelay(milliseconds)
     {
         dragQueryDelay = milliseconds;
